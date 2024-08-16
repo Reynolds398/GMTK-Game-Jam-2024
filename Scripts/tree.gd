50e0a8be04c7a4d17d84cd378ccae336d0d0b147
Reynolds398 <reynoldladestu@gmail.com>
2024-08-16 15:34:44 -0700

    Tree mechanics implemented
    Tree now can go to next stage when pressing space. Up to stage 7 for now.

---
+++ Scripts/tree.gd
@@ -0,0 +1,133 @@
+extends Node2D
+
+@export var CUR_TREE_STAGE = 0  # Variable to keep track of which stage the tree is in
+@export var MAX_TREE_STAGE = 7  # Variable that states the max stage for the tree
+
+@onready var camera = $TreeCamera  # Camera variable
+# Stages variable
+@onready var stage_1 = $"Stage 1"
+@onready var stage_2 = $"Stage 2"
+@onready var stage_3 = $"Stage 3"
+@onready var stage_4 = $"Stage 4"
+@onready var stage_5 = $"Stage 5"
+@onready var stage_6 = $"Stage 6"
+@onready var stage_7 = $"Stage 7"
+
+var current_stage = null  # Variable to keep track of the current stage object
+
+# Called when the node enters the scene tree for the first time.
+#func _ready():
+#	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	# For debugging purposes (press space to go to next stage)
+	if Input.is_action_just_pressed("ui_accept"):
+		if CUR_TREE_STAGE < MAX_TREE_STAGE:
+			CUR_TREE_STAGE += 1
+			next_stage(CUR_TREE_STAGE)
+
+# Function to set variables for stage 1
+func set_stage_1():
+	# Camera update here
+	camera.limit_bottom = 650
+	
+	# If stage has not been set
+	if current_stage == null:
+		stage_1.visible = true
+	else: # If stage has been set
+		stage_1.visible = true
+		current_stage.visible = false
+		current_stage = stage_1
+
+# Function to set variables for stage 2
+func set_stage_2():
+	# Camera update here
+	
+	# If stage has not been set
+	if current_stage == null:
+		stage_2.visible = true
+	else: # If stage has been set
+		stage_2.visible = true
+		current_stage.visible = false
+		current_stage = stage_2
+
+# Function to set variables for stage 3
+func set_stage_3():
+	# Camera update here
+	
+	# If stage has not been set
+	if current_stage == null:
+		stage_3.visible = true
+	else: # If stage has been set
+		stage_3.visible = true
+		current_stage.visible = false
+		current_stage = stage_3
+
+# Function to set variables for stage 4
+func set_stage_4():
+	# Camera update here
+	
+	# If stage has not been set
+	if current_stage == null:
+		stage_4.visible = true
+	else: # If stage has been set
+		stage_4.visible = true
+		current_stage.visible = false
+		current_stage = stage_4
+
+# Function to set variables for stage 5
+func set_stage_5():
+	# Camera update here
+	
+	# If stage has not been set
+	if current_stage == null:
+		stage_5.visible = true
+	else: # If stage has been set
+		stage_5.visible = true
+		current_stage.visible = false
+		current_stage = stage_5
+
+# Function to set variables for stage 6
+func set_stage_6():
+	# Camera update here
+	
+	# If stage has not been set
+	if current_stage == null:
+		stage_6.visible = true
+	else: # If stage has been set
+		stage_6.visible = true
+		current_stage.visible = false
+		current_stage = stage_6
+
+# Function to set variables for stage 7
+func set_stage_7():
+	# Camera update here
+	camera.limit_bottom = 670
+	
+	# If stage has not been set
+	if current_stage == null:
+		stage_7.visible = true
+	else: # If stage has been set
+		stage_7.visible = true
+		current_stage.visible = false
+		current_stage = stage_7
+
+# Function to deal with stage transitions
+func next_stage(current_stage_num):
+	match CUR_TREE_STAGE:
+		1:
+			set_stage_1()
+		2:
+			set_stage_2()
+		3:
+			set_stage_3()
+		4:
+			set_stage_4()
+		5:
+			set_stage_5()
+		6:
+			set_stage_6()
+		7:
+			set_stage_7()
