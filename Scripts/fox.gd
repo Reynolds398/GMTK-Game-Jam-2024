453047831ab94e7e064cb0871c0a631a9fd9d15c
chalkchugger <noelhasnuryadi@gmail.com>
2024-08-17 05:29:26 +0100

    replaced "grounded" with "beetle" and added "fox" and "woodpecker"
    flying enemies path not created yet.

--- Scripts/fox.gd
+++ Scripts/fox.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-@export var speed = 30
+@export var speed = 20
 @export var damage = 10
 
 func _ready():
@@ -8,6 +8,6 @@ func _ready():
 
 func _process(delta):
 	get_parent().set_progress(get_parent().get_progress() + speed*delta)
-	if get_parent().get_progress_ratio() == 1:
+	if get_parent().get_progress_ratio() == 0.8:
 		speed = 0
 		get_node("AnimatedSprite2D").play("attack")
