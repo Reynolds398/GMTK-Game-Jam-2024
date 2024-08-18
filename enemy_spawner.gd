e0bb01cb62e4c49eb629045eaa32f9325485ab64
chalkchugger <noelhasnuryadi@gmail.com>
2024-08-18 07:58:15 +0100

    changed scale of enemies

--- enemy_spawner.gd
+++ enemy_spawner.gd
@@ -27,7 +27,7 @@ var end_fly_r_left = Vector2.ZERO  # Variable to hold the end point of range fly
 
 ## Called when the node enters the scene tree for the first time.
 func _ready():
-	var test = beetle_m.instantiate()
+	var test = boar.instantiate()
 	test.init_start_end_pos(Vector2(27, 610), Vector2(522, 610))
 	test.RIGHT = true
 	add_child(test)
